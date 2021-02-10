3c01899599d7369880c3b72a8657f49aa244bef5
Mathews-Joby <78690832+Mathews-Joby@users.noreply.github.com>
2021-02-10 12:00:04 +0400

    Update ai_trials.py
    hey. this is an ai i made using python on which i am working right now.

--- ai_trials.py
+++ ai_trials.py
@@ -10,12 +10,17 @@ import pynput
 import pywhatkit
 from pynput.keyboard import Key, Controller
 
+# i have installed some liraries using the pip command
+# you can install these using the pip command pip install library name
+
 keyboard = Controller()
 engine = pyttsx3.init()
 while True:
+    #a loop so it goes on and on
     
 
     user = input("                                  YOU: ")
+    #taking input from the user
     
 
     if "play" in user:
@@ -53,6 +58,6 @@ while True:
 
 
 
-
+    #you can try these
     #os.startfile('chrome.exe')
     #os.startfile('Unity Hub.exe')
